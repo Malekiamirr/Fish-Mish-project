a7679731faae84b0dc4a17f324bc52045704a44d
Amir Maleki <realmalekiamir@gmail.com>
2022-12-29 10:42:06 +0330

    create draw function

--- src/app.cpp
+++ src/app.cpp
@@ -1,11 +1,32 @@
 #include "SFML/Graphics.hpp"
 #include <iostream>
 #include "Player.cpp"
+#include "draw.h"
 
 int main()
 {
 	Player player;
 	player.setName("Amir Maleki");
-	cout << player.getName();
+	cout << player.getName() << endl;
+	draw(3);
+
 	return 0;
 }
+// for (size_t column = 0; column < 8; column++)
+// {
+// 	cout << "-";
+// }
+// cout << endl;
+// for (size_t row = 0; row < 6; row++)
+// {
+// 	cout << "-";
+// 	for (size_t column = 0; column < 4; column++)
+// 	{
+// 		cout << " ";
+// 	}
+// 	cout << "-" << endl;
+// }
+// for (size_t column = 0; column < 8; column++)
+// {
+// 	cout << "-";
+// }
